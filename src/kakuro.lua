069616a157110166c95e7e9ce0fde3376ca6c8f3
James Petry <gavilancomun@gmail.com>
2016-06-18 16:31:21 +0100

    not working

--- src/kakuro.lua
+++ src/kakuro.lua
@@ -72,6 +72,39 @@ function print_r (t)
     print()
 end
 
+-- http://stackoverflow.com/questions/20325332/how-to-check-if-two-tablesobjects-have-the-same-value-in-lua
+
+function equals(o1, o2, ignore_mt)
+    if o1 == o2 then return true end
+    local o1Type = type(o1)
+    local o2Type = type(o2)
+    if o1Type ~= o2Type then return false end
+    if o1Type ~= 'table' then return false end
+
+    if not ignore_mt then
+        local mt1 = getmetatable(o1)
+        if mt1 and mt1.__eq then
+            --compare using built in method
+            return o1 == o2
+        end
+    end
+
+    local keySet = {}
+
+    for key1, value1 in pairs(o1) do
+        local value2 = o2[key1]
+        if value2 == nil or equals(value1, value2, ignore_mt) == false then
+            return false
+        end
+        keySet[key1] = true
+    end
+
+    for key2, _ in pairs(o2) do
+        if not keySet[key2] then return false end
+    end
+    return true
+end
+
 --
 
 -- kakuro solver
@@ -348,13 +381,13 @@ end
 function solveGrid(grid)
   local step1 = map(solveRow, grid)
   local step2 = transpose(step1)
-  local step3 = map(solveColumn(step2))
+  local step3 = map(solveColumn, step2)
   return transpose(step3)
 end
 
 function solver(grid)
   local g = solveGrid(grid)
-    if g == grid then
+    if equals(g, grid) then
       return g
     else
      print(drawGrid(g))
